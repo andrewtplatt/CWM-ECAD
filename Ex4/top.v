fc9446ecdf0f4197831344ad0352b5df5e645ced
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-01 15:38:45 +0000

    Started on Ex4.

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 
-// Student Name:
-// Date: 
+// Student Name: Andrew Platt
+// Date: 01/06/2020
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
@@ -15,4 +15,30 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module dicethrow(clk, rst, button, throw);
+	
+	// I/O
+    input clk, rst, button;
+    output [2:0] throw;
+	wire thrown;
+	
+    //logic
+    assign thrown = 1'b0;
+	always @(posedge clk or posedge rst)
+	begin
+		if (rst)
+			throw = 3'b000;
+		else if (button)
+			if throw[2]&throw[1]
+				throw = 3'b001;
+			else
+			    throw = throw + 1;
+		else
+			thrown = 1'b1;
+	end
+
+endmodule
 
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,42 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Electronic Dice
-// Student Name:
-// Date: 
+// Student Name: Andrew Platt
+// Date: 01/06/2020
 //
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+
+    parameter CLK_PERIOD = 10;
+
+	reg clk, rst, button;
+	wire [2:0] throw;
+	
+	initial begin
+    clk = 0;
+    forever
+        #(CLK_PERIOD/2) clk = ~clk;
+    end
+    
+    initial begin
+    	rst = 1;
+    	button = 1;
+    	#CLK_PERIOD
+    	rst = 0;
+    	forever begin
+        	#5*CLK_PERIOD
+        	rst = 1;
+        	#3*CLK_PERIOD
+        	button = 0;
+    	end
+    end
+    
+    dicethrow testdicethrow(clk, rst, button, throw);
+    
+    
+    

1843db3cfad8c0a880363bfb9ecceccf1636adfb
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-02 09:20:52 +0000

    Improved testbench for Ex. 3

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -17,7 +17,7 @@ module top_tb(
     parameter CLK_PERIOD = 10;
 
 //Todo: Regitsers and wires
-    reg clk, rst, enable, direction;
+    reg clk, rst, enable, direction, err;
     wire [7:0] counter_out;
 
 //Todo: Clock generation
@@ -29,25 +29,55 @@ module top_tb(
 
 //Todo: User logic
     initial begin
-        rst = 1;
-        enable = 0;
-        direction = 1;
-        forever begin
+        rst <= 1;
+        enable <= 0;
+        direction <= 1;
+        err <= 0;
          #CLK_PERIOD
-         rst = ~rst;
+         rst <= 0;
          #CLK_PERIOD
-         enable = 1;
-         #CLK_PERIOD
-         direction = 0;
-         #CLK_PERIOD
-         direction = 1;
-         #CLK_PERIOD
-         enable = 0;
-       end
+         enable <= 1;
+         #(2*CLK_PERIOD)
+         if (counter_out != 2) begin
+         	$display("***TEST FAILED! expected out=2, actual out=%d ***", counter_out);
+         	err <= 1;
+         end
+         enable <= 0;
+         #(2*CLK_PERIOD)
+         if (counter_out != 2) begin
+         	$display("***TEST FAILED! expected out=2, actual out=%d ***", counter_out);
+         	err <= 1;
+         end
+         direction <= 0;
+         enable <= 1;
+         #(2*CLK_PERIOD)
+         if (counter_out != 0) begin
+         	$display("***TEST FAILED! expected out=0, actual out=%d ***", counter_out);
+         	err <= 1;
+         end
+         direction <= 1;
+         #(4*CLK_PERIOD)
+         if (counter_out != 4) begin
+         	$display("***TEST FAILED! expected out=0, actual out=%d ***", counter_out);
+         	err <= 1;
+         end
+         rst <= 1;
+         #(3*CLK_PERIOD)
+         if (counter_out != 0) begin
+         	$display("***TEST FAILED! expected out=0, actual out=%d ***", counter_out);
+         	err <= 1;
+         end
+         enable <= 0;
     end
     
 //Todo: Finish test, check for success
-    
+    initial begin
+    	#(17*CLK_PERIOD)
+    	if (err)
+         	$display("***TEST FAILED! expected final out=0, actual out=%d ***", counter_out);
+        else 
+         	$display("***TEST PASSED! expected out=0, actual out=%d ***", counter_out);
+     end
 //Todo: Instantiate counter module
     counter test_counter(clk, rst, enable, direction, counter_out);
     

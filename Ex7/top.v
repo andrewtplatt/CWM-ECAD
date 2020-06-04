18e6e2b4b21292af7bb28ff5810cf7413a94466f
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-04 12:15:40 +0000

    Started Ex.7. Memory working.

--- Ex7/top.v
+++ Ex7/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #7
-// Student Name:
-// Date: 
+// Student Name: Andrew Platt
+// Date: 04/06/2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory.
@@ -12,5 +12,34 @@
 //  outputs:
 //           result[4:0]
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns/100ps
+
+module times_tables(
+	input clk, enable,
+	input [2:0] a, b,
+	output [4:0] result);
+
+	wire [5:0] ab;
+	
+	assign ab = {a, b};
+	
+	mybram lookup_table (
+  .clka(clk),     // input wire clka
+  .ena(enable),   // input wire ena
+  .wea(0),      // input wire [0 : 0] wea
+  .addra(ab),     // input wire [5 : 0] addra
+  .dina(result),    // input wire [5 : 0] dina
+  .douta(result)  // output wire [4 : 0] douta
+);
+	
+	//mybram lookuptable(ab, result);
+	
+endmodule
+
+
+
+
+
 
 
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -7,3 +7,28 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+
+module times_table_tester();
+
+	parameter CLK_PERIOD = 10;
+	reg clk, enable;
+	reg [2:0] a, b;
+	wire [4:0] result;
+	
+	// clock
+    initial begin
+    clk = 0;
+    forever
+        #(CLK_PERIOD/2) clk = ~clk;
+    end
+	
+	initial begin
+		enable = 1;
+		a = 3'b001;
+		b = 3'b111;
+	end
+	
+	times_tables mytimestables(.clk(clk), .a(a), .b(b), .enable(enable), .result(result));
+	
+endmodule

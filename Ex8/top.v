2781c828f729092e44f6ff953b0697948c5112ec
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-04 17:03:57 +0000

    started on Ex. 8. BRAM needs instantiating.

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
-// Date: 
+// Student Name: Andrew Platt
+// Date: 04/06/2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory and AXI-4-lite interface.
@@ -12,3 +12,48 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns/100ps
+
+module times_tables(
+	input clk, enable, rst,
+	input [2:0] a, b,
+	output [4:0] result);
+
+	wire [5:0] ab;
+	wire [31:0] addr, value;
+
+	assign ab = {a, b};
+	assign addr = {26'b00000000000000000000000000, ab};
+	assign result = value[4:0];
+
+	mybram lookup_table (
+  .rsta_busy(0),          // output wire rsta_busy
+  .rstb_busy(0),          // output wire rstb_busy
+  .s_aclk(clk),                // input wire s_aclk
+  .s_aresetn(rst),          // input wire s_aresetn
+  .s_axi_awaddr(addr),    // input wire [31 : 0] s_axi_awaddr
+  .s_axi_awvalid(1),  // input wire s_axi_awvalid
+  .s_axi_awready(0),  // output wire s_axi_awready
+  .s_axi_wdata(addr),      // input wire [31 : 0] s_axi_wdata
+  .s_axi_wstrb(4'b0000),      // input wire [3 : 0] s_axi_wstrb
+  .s_axi_wvalid(0),    // input wire s_axi_wvalid
+  .s_axi_wready(0),    // output wire s_axi_wready
+  .s_axi_bresp(OKAY),      // output wire [1 : 0] s_axi_bresp
+  .s_axi_bvalid(0),    // output wire s_axi_bvalid
+  .s_axi_bready(0),    // input wire s_axi_bready
+  .s_axi_araddr(addr),    // input wire [31 : 0] s_axi_araddr
+  .s_axi_arvalid(1),  // input wire s_axi_arvalid
+  .s_axi_arready(1),  // output wire s_axi_arready
+  .s_axi_rdata(value),      // output wire [31 : 0] s_axi_rdata
+  .s_axi_rresp(OKAY),      // output wire [1 : 0] s_axi_rresp
+  .s_axi_rvalid(1),    // output wire s_axi_rvalid
+  .s_axi_rready(1)    // input wire s_axi_rready
+);
+		
+endmodule
+		
+		
+		
+		
+
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,74 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8 - Times Table using AXI interface
-// Student Name:
-// Date: 
+// Student Name: Andrew Platt
+// Date: 04/06/2020
 //
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+
+module times_table_tester();
+
+	parameter CLK_PERIOD = 10;
+	reg clk, enable, err, rst;
+	reg [2:0] a, b;
+	wire [4:0] result, expected_result;
+	
+	// clock
+    initial begin
+    clk = 0;
+    forever
+        #(CLK_PERIOD/2) clk = ~clk;
+    end
+	
+	assign #(2*CLK_PERIOD) expected_result = (a == 3'b000 || b == 3'b000) ? 0:
+							 (a == 3'b001) ? b:
+							 (b == 3'b001) ? a:
+							 (a == 3'b010) ? b+b:
+							 (b == 3'b010) ? a+a:
+							 (a == 3'b011) ? b+b+b:
+							 (b == 3'b011) ? a+a+a: 
+							 (a == 3'b100) ? b+b+b+b:
+							 (b == 3'b100) ? a+a+a+a:
+							 (a == 3'b101) ? b+b+b+b+b:
+							 (b == 3'b101) ? a+a+a+a+a:
+							 (a == 3'b110) ? b+b+b+b+b+b:
+							 (b == 3'b110) ? a+a+a+a+a+a:
+							 (a == 3'b111) ? b+b+b+b+b+b+b:
+							 a+a+a+a+a+a+a;
+
+	initial begin
+		#(CLK_PERIOD/2)
+		rst = 0;
+		err = 0;
+		enable = 1;
+		a = 3'b000;
+		b = 3'b000;
+		forever begin
+		    #(3*CLK_PERIOD)
+			if (result != expected_result) begin
+				$display("*** TEST FAILED! Expected result: %d. Result: %d ***", expected_result, result);
+				err = 1;
+			end
+			if (b == 3'b111) begin
+				b = 3'b000;
+				a = a + 3'b001;
+			end
+			else
+				b = b + 3'b001;
+		end
+	end
+	
+	initial begin
+	   #(79*CLK_PERIOD)
+	   if (err)
+	       $display("*** TEST FAILED! ***");
+	   else
+	       $display("*** TEST PASSED! ***");
+	end
+
+	times_tables mytimestables(.clk(clk), .a(a), .b(b), .enable(enable), .result(result), .rst(rst));
+
+endmodule
